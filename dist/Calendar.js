70f4bb8c5d6a3decef065e0af5d35227600f68de
MoatazMohamed <moatazmohamedahmed606@gmail.com>
2022-09-29 15:48:11 +0200

    update moment format to handle exception

--- dist/Calendar.js
+++ dist/Calendar.js
@@ -63,62 +63,62 @@ const CalendarTemplate = ({
     1: {
       lastDay: 31,
       month: "January",
-      firstDay: moment(`01/01/${year}`)
+      firstDay: moment(`01/01/${year}`, "MM-DD-YYYY")
     },
     2: {
       lastDay: year % 4 === 0 ? 29 : 28,
       month: "February",
-      firstDay: moment(`02/01/${year}`)
+      firstDay: moment(`02/01/${year}`, "MM-DD-YYYY")
     },
     3: {
       lastDay: 31,
       month: "March",
-      firstDay: moment(`03/01/${year}`)
+      firstDay: moment(`03/01/${year}`, "MM-DD-YYYY")
     },
     4: {
       lastDay: 30,
       month: "April",
-      firstDay: moment(`04/01/${year}`)
+      firstDay: moment(`04/01/${year}`, "MM-DD-YYYY")
     },
     5: {
       lastDay: 31,
       month: "May",
-      firstDay: moment(`05/01/${year}`)
+      firstDay: moment(`05/01/${year}`, "MM-DD-YYYY")
     },
     6: {
       lastDay: 30,
       month: "June",
-      firstDay: moment(`06/01/${year}`)
+      firstDay: moment(`06/01/${year}`, "MM-DD-YYYY")
     },
     7: {
       lastDay: 31,
       month: "July",
-      firstDay: moment(`07/01/${year}`)
+      firstDay: moment(`07/01/${year}`, "MM-DD-YYYY")
     },
     8: {
       lastDay: 31,
       month: "August",
-      firstDay: moment(`08/01/${year}`)
+      firstDay: moment(`08/01/${year}`, "MM-DD-YYYY")
     },
     9: {
       lastDay: 30,
       month: "September",
-      firstDay: moment(`09/01/${year}`)
+      firstDay: moment(`09/01/${year}`, "MM-DD-YYYY")
     },
     10: {
       lastDay: 31,
       month: "October",
-      firstDay: moment(`10/01/${year}`)
+      firstDay: moment(`10/01/${year}`, "MM-DD-YYYY")
     },
     11: {
       lastDay: 30,
       month: "November",
-      firstDay: moment(`11/01/${year}`)
+      firstDay: moment(`11/01/${year}`, "MM-DD-YYYY")
     },
     12: {
       lastDay: 31,
       month: "December",
-      firstDay: moment(`12/01/${year}`)
+      firstDay: moment(`12/01/${year}`, "MM-DD-YYYY")
     }
   });
 
